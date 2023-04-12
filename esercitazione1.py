048f569d3d1723e544be0db94b21211679ca0c1d
DavideDipace <dipace.davide@iisgalvanimi.edu.it>
2023-04-12 12:24:44 +0000

    primo esercizio

---
+++ esercitazione1.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('esercitazione1.html')
+
+@app.route('/inglese', methods = ['GET'])
+def search():
+    import pandas as pd
+    Dati_Film = pd.read_csv('https://raw.githubusercontent.com/wtitze/3E/main/2010.csv', delimiter = ';')
+    risultato = Dati_Film[Dati_Film['Language']== 'English']
+    if len(risultato) == 0:
+        table = 'Film non trovato'
+    else:
+        table = risultato.to_html()
+    return render_template('table.html', tabella = table)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
